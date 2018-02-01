66af069594eb3da1e21320dd271a6bbddd211d9b
Michael Droettboom <mdboom@gmail.com>
2018-02-01 14:40:03 -0500

    Add custom output handler

---
+++ custom-output-handler.js
@@ -0,0 +1,11 @@
+const GeoLocationOutputHandler = {
+    shouldHandle: value => {
+        return 'lat' in value && 'lon' in value
+    }
+
+    render: value => {
+        return '<img src="https://maps.googleapis.com/maps/api/staticmap?center=' + value.lat + ',' + value.lon + '&zoom=17&size=600x300&maptype=roadmap"/>'
+    }
+}
+
+window.nb_api.addOutputHandler(GeoLocationOutputHandler)

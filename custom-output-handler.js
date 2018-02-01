6f638bdbce330c4a46cd3ba8a674fa4d94829ffd
Michael Droettboom <mdboom@gmail.com>
2018-02-01 15:23:16 -0500

    Syntax fixes

--- custom-output-handler.js
+++ custom-output-handler.js
@@ -1,12 +1,10 @@
 const GeoLocationOutputHandler = {
     shouldHandle: function(value) {
-        alert(typeof value)
         return (typeof value === 'object') && 'lat' in value && 'lon' in value
     },
 
     render: function(value) {
-        alert("FOO")
-        '<img src="https://maps.googleapis.com/maps/api/staticmap?center=' + value.lat + ',' + value.lon + '&zoom=17&size=600x300&maptype=roadmap"/>'
+        return '<img src="https://maps.googleapis.com/maps/api/staticmap?center=' + value.lat + ',' + value.lon + '&zoom=17&size=600x300&maptype=roadmap"/>'
     }
 }
 
